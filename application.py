b0b1e383d1a6ea264b2adc00c606bc6ecef8c3c2
36Lie <91595393+36Lie@users.noreply.github.com>
2024-09-04 07:47:44 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,17 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def main_page():
-    if request.method == 'GET':
-        text = "ここに結果が出力されます"
-        return render_template("page.html",text=text)
-    elif request.method == 'POST':
-        name = request.form["name"]
-        text = "こんにちは" + name + "さん"
-        return render_template("page.html",text=text)
-
-## 実行
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def hello_world():
+    return "<p>Hello world!</p>"
